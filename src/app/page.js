0a53850ef429807d8d86fa2b27b0cdba9f265411
Invers3 <manyandamariano@hotmail.com>
2025-02-14 21:20:37 -0500

    Add posts.js
    Testing post

--- src/app/page.js
+++ src/app/page.js
@@ -3,23 +3,16 @@ import path from 'path'
 
 export default function Home() {
 
-  const filePath = path.join(process.cwd(), 'src', 'posts', 'entrances.json')
-  const fileContent = fs.readFileSync(filePath, 'utf8')
-  const posts = JSON.parse(fileContent)
+  const filePath = path.join(process.cwd(), 'src', 'posts')
+  const files = fs.readdirSync(filePath)
 
   return <div>
     <h1>Bienvenidos a Playec</h1>
     <div>
       <ul>
-      {posts.map((post) => (
-          <li key={post.slug}>
-            <a href={`/post/${post.slug}`}>
-              <h2>{post.title}</h2>
-              <p>{post.description}</p>
-              <p>Fecha: {post.date}</p>
-            </a>
-          </li>
-        ))}
+      {files.map(function(post){
+        return <li>{post}</li>
+      })}
       </ul>
     </div>
   </div>
