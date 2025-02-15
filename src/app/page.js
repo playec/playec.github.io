3ff7ef1a7802a2b947f9885eea58a61730d0b3a8
Invers3 <manyandamariano@hotmail.com>
2025-02-14 19:08:30 -0500

    Upload posts
    Testing posting

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,26 @@
 import fs from 'fs'
+import path from 'path'
 
-export default function Page() {
-  return <h1>Bienvenidos a Playec</h1>
+export default function Home() {
+
+  const filePath = path.join(process.cwd(), 'src', 'posts', 'entrances.json')
+  const fileContent = fs.readFileSync(filePath, 'utf8')
+  const posts = JSON.parse(fileContent)
+
+  return <div>
+    <h1>Bienvenidos a Playec</h1>
+    <div>
+      <ul>
+      {posts.map((post) => (
+          <li key={post.slug}>
+            <a href={`/post/${post.slug}`}>
+              <h2>{post.title}</h2>
+              <p>{post.description}</p>
+              <p>Fecha: {post.date}</p>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
 }
