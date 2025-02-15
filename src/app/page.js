98b9cffd46549ce5ccc04c3188aa77ac01e79e4e
Invers3 <manyandamariano@hotmail.com>
2025-02-14 22:10:57 -0500

    Update page.js
    Testing

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,23 @@ import path from 'path'
 
 export default function Home() {
 
-  const filePath = path.join(process.cwd(), 'src', 'posts')
-  const files = fs.readdirSync(filePath)
+  const filePath = path.join(process.cwd(), 'public', 'posts.json')
+  let posts = []
+  try{
 
+    const fileContent = fs.readFileSync(filePath, 'utf8')
+    posts = JSON.parse(fileContent)
+
+  }catch(e){
+    posts = []
+  }
+  
   return <div>
     <h1>Bienvenidos a Playec</h1>
     <div>
       <ul>
-      {files.map(function(post){
-        return <li>{post}</li>
+      {posts.map(function(post){
+        return <li>{post.title}</li>
       })}
       </ul>
     </div>
