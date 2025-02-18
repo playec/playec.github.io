85919477a0af5633ea24614f06b80fa1ec844017
Invers3 <manyandamariano@hotmail.com>
2025-02-17 21:09:54 -0500

    Update footer.js
    Testing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Header(){
             <ul className="list">
                 <li><a href="/ubicacion">Ubicación</a></li>
                 <li><a target="_blank" href="https://www.facebook.com/playecdigi">Facebook</a></li>
-                <li><a target="_blank" href="https://api.whatsapp.com/send?phone=593982615237">WhatsApp</a></li>
+                <li><a target="_blank" href="https://api.whatsapp.com/send?phone=59395894018">WhatsApp</a></li>
             </ul>
             <br></br>
             <sub>© 2025 Playec tienda local Minorista de videojuegos.</sub>
